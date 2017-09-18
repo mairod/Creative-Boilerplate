ccaccb6cdb884db838ed423249d3f8744f497a1d
Dorian <dorianlods@gmail.com>
2017-09-19 00:16:56 +0200

    fix

--- settings.config.js
+++ settings.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     browsersTarget: ["last 2 versions"], // Browserlist avaliable here : https://github.com/ai/browserslist
     port: 3000,
-    https: false,
+    https: true,
     sourceMap: true,
     shared: true,
     inline: true,
